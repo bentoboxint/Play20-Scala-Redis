1332cbba801e71ad04043d1cbe244b38b00303d4
Calvin Wong <Calvin@Calvins-MacBook-Pro.local>
2012-08-06 10:45:13 -0700

    Version number bumped

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play20-scala-redis"
-    val appVersion      = "0.2"
+    val appVersion      = "0.3"
 
     val appDependencies = Seq(
         // Add your project dependencies here,
